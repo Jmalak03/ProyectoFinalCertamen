631bed59ced0e699694806708b16a0cd941b1fc3
Gabriel027-ux <gabrielcalde16@gmail.com>
2023-11-02 20:07:19 -0600

    votacion visual

--- Controllers/VotacionController.cs
+++ Controllers/VotacionController.cs
@@ -1,6 +1,27 @@
-﻿namespace APPCRUD.Controllers
+﻿using FreeSql;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APPCRUD.Controllers
 {
-    public class VotacionController
+    public class VotacionController : Controller
     {
+        private DbContext _context;
+
+        public VotacionController(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Votar(int Id)
+        {
+            var concursante = _context.Concursante.FirstOrDefault(c => c.Id == Id);
+            if (concursante != null)
+            {
+                concursante.Votos++;
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
--- Models/VotacionModel.cs
+++ Models/VotacionModel.cs
@@ -1,6 +1,21 @@
-﻿namespace APPCRUD.Models
+﻿using FreeSql.Extensions;
+
+namespace APPCRUD.Models
 {
     public class VotacionModel
     {
+        public class Concursante
+        {
+            public int ConcursanteId { get; set; }
+            public string Concursantes { get; set; }
+            public int Votos { get; set; }
+        }
+
+        public class Voto
+        {
+            public int VotoId { get; set; }
+            public string ConcursanteId { get; set; }
+            public int UserId { get; set; }
+        }
     }
 }
